e3d6c3cc2e3feb37ba61f15f3ac1c48c0b57a0d8
AsuosOnurb <brunoadns@gmail.com>
2026-09-13 19:44:02 +0100

    Configure GitHub Pages project path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,3 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-export default defineConfig({ site: 'https://syntaxerror.github.io', integrations: [sitemap()] });
+export default defineConfig({ site: 'https://asuosonurb.github.io/learning-by-heart', base: '/learning-by-heart', integrations: [sitemap()] });
